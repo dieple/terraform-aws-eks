3d83de8ad15e677a783054823f9cddc8af33c11f
Diep Le <diep.le@river-island.com>
2019-06-12 10:20:41 +0100

    add further policies to workers node

--- pods.tf
+++ pods.tf
@@ -73,6 +73,11 @@ data "aws_iam_policy_document" "k8s_pods_assume_role_policy" {
       type        = "Service"
       identifiers = ["ec2.amazonaws.com"]
     }
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
   }
 }
 
@@ -95,29 +100,75 @@ data "aws_iam_policy_document" "k8s_pods_policy" {
     effect = "Allow"
 
     actions = [
+      "ec2:AllocateAddress",
+      "ec2:AssignPrivateIpAddresses",
+      "ec2:Associate*",
+      "ec2:AttachInternetGateway",
+      "ec2:AttachNetworkInterface",
+      "ec2:AuthorizeSecurityGroupEgress",
       "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:CreateDefaultSubnet",
+      "ec2:CreateDhcpOptions",
+      "ec2:CreateEgressOnlyInternetGateway",
+      "ec2:CreateInternetGateway",
+      "ec2:CreateLaunchTemplate",
+      "ec2:CreateLaunchTemplateVersion",
+      "ec2:CreateNatGateway",
+      "ec2:CreateNetworkInterface",
+      "ec2:CreateRoute",
+      "ec2:CreateRouteTable",
       "ec2:CreateSecurityGroup",
+      "ec2:CreateSubnet",
       "ec2:CreateTags",
-      "ec2:DeleteTags",
+      "ec2:CreateVolume",
+      "ec2:CreateVpc",
+      "ec2:DeleteDhcpOptions",
+      "ec2:DeleteEgressOnlyInternetGateway",
+      "ec2:DeleteInternetGateway",
+      "ec2:DeleteLaunchTemplate",
+      "ec2:DeleteLaunchTemplateVersions",
+      "ec2:DeleteNatGateway",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DeleteRoute",
+      "ec2:DeleteRouteTable",
       "ec2:DeleteSecurityGroup",
+      "ec2:DeleteSubnet",
+      "ec2:DeleteTags",
+      "ec2:DeleteVolume",
+      "ec2:DeleteVpc",
+      "ec2:DeleteVpnGateway",
+      "ec2:Describe*",
       "ec2:DescribeAccountAttributes",
       "ec2:DescribeAddresses",
-      "ec2:DescribeInstances",
       "ec2:DescribeInstanceStatus",
+      "ec2:DescribeInstances",
       "ec2:DescribeInternetGateways",
+      "ec2:DescribeLaunchTemplateVersions",
+      "ec2:DescribeLaunchTemplates",
+      "ec2:DescribeNetworkInterfaceAttribute",
       "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeRouteTables",
       "ec2:DescribeSecurityGroups",
       "ec2:DescribeSubnets",
       "ec2:DescribeTags",
       "ec2:DescribeVpcs",
-      "ec2:DescribeRouteTables",
-      "ec2:DescribeVpcs",
+      "ec2:DetachInternetGateway",
+      "ec2:DetachNetworkInterface",
+      "ec2:DetachVolume",
+      "ec2:Disassociate*",
+      "ec2:GetLaunchTemplateData",
       "ec2:ModifyInstanceAttribute",
+      "ec2:ModifyLaunchTemplate",
       "ec2:ModifyNetworkInterfaceAttribute",
+      "ec2:ModifySubnetAttribute",
+      "ec2:ModifyVpcAttribute",
+      "ec2:ModifyVpcEndpoint",
+      "ec2:ReleaseAddress",
+      "ec2:RevokeSecurityGroupEgress",
       "ec2:RevokeSecurityGroupIngress",
-      "ec2:CreateNetworkInterface",
-      "ec2:DeleteNetworkInterface",
-      "ec2:DescribeNetworkInterfaceAttribute",
+      "ec2:RunInstances",
+      "ec2:UpdateSecurityGroupRuleDescriptionsEgress",
+      "ec2:UpdateSecurityGroupRuleDescriptionsIngress",
     ]
 
     resources = ["*"]
@@ -176,9 +227,31 @@ data "aws_iam_policy_document" "k8s_pods_policy" {
     effect = "Allow"
 
     actions = [
+      "iam:AddRoleToInstanceProfile",
+      "iam:AttachRolePolicy",
+      "iam:CreateInstanceProfile",
+      "iam:CreatePolicy",
+      "iam:CreatePolicyVersion",
+      "iam:CreateRole",
       "iam:CreateServiceLinkedRole",
+      "iam:DeleteInstanceProfile",
+      "iam:DeletePolicy",
+      "iam:DeleteRole",
+      "iam:DeleteRolePolicy",
+      "iam:DeleteServiceLinkedRole",
+      "iam:DetachRolePolicy",
+      "iam:GetInstanceProfile",
+      "iam:GetPolicy",
+      "iam:GetPolicyVersion",
+      "iam:GetRole",
+      "iam:GetRolePolicy",
       "iam:GetServerCertificate",
+      "iam:List*",
       "iam:ListServerCertificates",
+      "iam:PassRole",
+      "iam:PutRolePolicy",
+      "iam:RemoveRoleFromInstanceProfile",
+      "iam:UpdateAssumeRolePolicy",
     ]
 
     resources = ["*"]
@@ -254,10 +327,22 @@ data "aws_iam_policy_document" "worker_autoscaling" {
     effect = "Allow"
 
     actions = [
+      "autoscaling:AttachInstances",
+      "autoscaling:CreateAutoScalingGroup",
+      "autoscaling:CreateLaunchConfiguration",
+      "autoscaling:CreateOrUpdateTags",
+      "autoscaling:DeleteAutoScalingGroup",
+      "autoscaling:DeleteLaunchConfiguration",
+      "autoscaling:DeleteTags",
+      "autoscaling:Describe*",
       "autoscaling:DescribeAutoScalingGroups",
       "autoscaling:DescribeAutoScalingInstances",
       "autoscaling:DescribeLaunchConfigurations",
       "autoscaling:DescribeTags",
+      "autoscaling:DetachInstances",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:SuspendProcesses",
+      "autoscaling:UpdateAutoScalingGroup",
       "ec2:DescribeLaunchTemplateVersions",
     ]
 
